8def3bc8587c71c079b1b5d74799297e6f31c864
Marco <marcosinisa.mihajlovic@gmail.com>
2021-05-05 15:17:41 +0200

    Test laravel
    fatto un controller

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return '<h1>Hello World</h1>';
+});
+
+Route::get('/users/{id}/{name}', function ($id, $name) {
+    return 'Questo utente è '.$name.' ed il suo id è '.$id;
+});
